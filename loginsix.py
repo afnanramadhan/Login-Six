6f7a4a0458a5e9575ed5e32bad9c4bce1e5978cf
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-01-07 01:06:34 +0700

    update readfile

--- loginsix.py
+++ loginsix.py
@@ -3,30 +3,36 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import time
+import keyboard
 
+
+#read nim and password from file
+f = open('nimPass.txt', 'r')
+nimPass = f.readlines()
+nimPass = list(map(lambda x:x.strip(),nimPass))
+
+
+
+#open browser
 driver = webdriver.Chrome()
 driver.get('https://akademik.itb.ac.id/home')
 driver.maximize_window()
-# time.sleep(2)
 
+#click login button
 login_btn = driver.find_element(By.ID, 'login')
 login_btn.click()
-# time.sleep(2)
 
+#click login with INA button
 login_ina_btn = driver.find_element(By.LINK_TEXT, 'Login dengan INA')
 login_ina_btn.click()
-# time.sleep(2)
 
-with open('Login-Six/pass.txt', 'r') as x:
-    password = x.read()
-with open('Login-Six/nim.txt', 'r') as x:
-    nim = x.read()
-
+#input nim and password
 uname = driver.find_element(By.NAME, 'username')
-uname.send_keys(nim)
+uname.send_keys(nimPass[0])
 pwd = driver.find_element(By.NAME, 'password')
-pwd.send_keys(password)
-# time.sleep(2)
+pwd.send_keys(nimPass[1])
 submit = driver.find_element(By.NAME, 'submit')
 submit.click()
-time.sleep(100)
+
+while keyboard.is_pressed('q') == False:
+    continue
