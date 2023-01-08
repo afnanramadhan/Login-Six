982d6ac090ce59471bbbec1eaa1314b01e46903d
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-01-08 20:55:49 +0700

    dah jadi yaiyy

--- loginsix.py
+++ loginsix.py
@@ -1,38 +1,40 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-import time
 import keyboard
 
 
 #read nim and password from file
 f = open('nimPass.txt', 'r')
 nimPass = f.readlines()
-nimPass = list(map(lambda x:x.strip(),nimPass))
+nimPass = list(map(lambda x:x.strip(),nimPass)) #delete \n
 
 
+def login(driver):
+    #open browser
+    
+    driver.get('https://akademik.itb.ac.id/home')
+    driver.maximize_window()
 
-#open browser
-driver = webdriver.Chrome()
-driver.get('https://akademik.itb.ac.id/home')
-driver.maximize_window()
+    #click login button
+    login_btn = driver.find_element(By.ID, 'login')
+    login_btn.click()
 
-#click login button
-login_btn = driver.find_element(By.ID, 'login')
-login_btn.click()
+    #click login with INA button
+    login_ina_btn = driver.find_element(By.LINK_TEXT, 'Login dengan INA')
+    login_ina_btn.click()
 
-#click login with INA button
-login_ina_btn = driver.find_element(By.LINK_TEXT, 'Login dengan INA')
-login_ina_btn.click()
+    #input nim and password
+    uname = driver.find_element(By.NAME, 'username')
+    uname.send_keys(nimPass[0])
+    pwd = driver.find_element(By.NAME, 'password')
+    pwd.send_keys(nimPass[1])
+    submit = driver.find_element(By.NAME, 'submit')
+    submit.click()
 
-#input nim and password
-uname = driver.find_element(By.NAME, 'username')
-uname.send_keys(nimPass[0])
-pwd = driver.find_element(By.NAME, 'password')
-pwd.send_keys(nimPass[1])
-submit = driver.find_element(By.NAME, 'submit')
-submit.click()
 
-while keyboard.is_pressed('q') == False:
-    continue
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    login(driver)
+    
+    while keyboard.is_pressed('q') == False:
+        continue
--- tandaiHadir.py
+++ tandaiHadir.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import time
 import keyboard
+from loginsix import login
 
 f = open('tandaiHadir.txt', 'r')
 tglMK = f.readlines()
@@ -12,32 +12,9 @@ tglMK = list(map(lambda x:x.strip(),tglMK))
 
 
 driver = webdriver.Chrome()
-driver.get('https://akademik.itb.ac.id/home')
-driver.maximize_window()
-# time.sleep(2)
+login(driver)
 
-login_btn = driver.find_element(By.ID, 'login')
-login_btn.click()
-# time.sleep(2)
-
-login_ina_btn = driver.find_element(By.LINK_TEXT, 'Login dengan INA')
-login_ina_btn.click()
-# time.sleep(2)
-
-# with open('Login-Six/pass.txt', 'r') as x:
-#     password = x.read()
-# with open('Login-Six/nim.txt', 'r') as x:
-#     nim = x.read()
-
-uname = driver.find_element(By.NAME, 'username')
-uname.send_keys('13521011')
-pwd = driver.find_element(By.NAME, 'password')
-pwd.send_keys('mhsITB109193')
-# time.sleep(2)
-submit = driver.find_element(By.NAME, 'submit')
-submit.click()
-# time.sleep(2)
-
+#open menu class
 kelas = driver.find_element(By.LINK_TEXT, 'Kelas')
 kelas.click()
 
@@ -50,46 +27,36 @@ sem1.click()
 des = driver.find_element(By.LINK_TEXT, 'Nov 2022')
 des.click()
 
-
+# select calender
 kalender = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/table/tbody")
 print(kalender.is_enabled())
 
-
-
+# bagi kalender jadi baris
 row = kalender.find_elements(By.TAG_NAME, 'tr')
 
-print(type(row))
 for i in row:
-    # print(i.text)
-    if tglMK[0] in i.text:
-        baris = i.find_elements(By.TAG_NAME, 'td')
-        break
+    #bagi baris jadi kolo,
+    col = i.find_elements(By.TAG_NAME, 'td')
+    for j in col:
+        #cari tanggal dari tiap kolom per baris
+        tgll = j.find_element(By.TAG_NAME, 'div')
+        if tglMK[0] in tgll.text:
+            press = j.find_element(By.PARTIAL_LINK_TEXT, tglMK[1])
+            break
 
-for i in baris:
-    if tglMK[0] in i.text:
-        tgl = i.find_elements(By.TAG_NAME, 'div')
-        break
+press.click()
 
-for i in tgl:
-    if tglMK[1] in i.text:
-        pencet = i.find_element(By.PARTIAL_LINK_TEXT, tglMK[1])
-        break
-pencet.click()
+# klik tandai hadir
+try:
+    tandaiHadir = driver.find_element(By.LINK_TEXT, 'Tandai Hadir')
+    tandaiHadir.click()
+    # driver.get('https://www.youtube.com/watch?v=13ARO0HDZsQ')
+    yutup = driver.execute_script('''window.open("https://www.youtube.com/watch?v=13ARO0HDZsQ","_blank");''')
+except:
+    print('Tidak ada pertemuan')
+    # driver.get('https://www.youtube.com/watch?v=pSUydWEqKwE')
+    yutup = driver.execute_script('''window.open("https://www.youtube.com/watch?v=V37TaRdVUQY","_blank");''')
 
-# tandaiHadir = driver.find_element(By.LINK_TEXT, 'Tandai Hadir')
-# tandaiHadir.click()
-
-print("selesai")
-# pencet.click()
-# pencet = driver.find_element(By.PARTIAL_LINK_TEXT, ya)
-# action = ActionChains(driver)
-# action.double_click(pencet).perform()
-
-# matdis1 = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/table/tbody/tr[1]/td[1]/div[2]/div[1]/a")
-# matdis1.click()
-
-# tandaiHadir = driver.find_element(By.LINK_TEXT, 'Tandai Hadir')
-# tandaiHadir.click()
 
 while keyboard.is_pressed('Esc') == False:
     continue
