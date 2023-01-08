35518bb81c945ba8723aa8bf7864b5b16b9bd49e
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-01-08 08:19:42 +0700

    hampir jadi

--- tandaiHadir.py
+++ tandaiHadir.py
@@ -6,6 +6,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import keyboard
 
+f = open('tandaiHadir.txt', 'r')
+tglMK = f.readlines()
+tglMK = list(map(lambda x:x.strip(),tglMK))
+
+
 driver = webdriver.Chrome()
 driver.get('https://akademik.itb.ac.id/home')
 driver.maximize_window()
@@ -36,11 +41,46 @@ submit.click()
 kelas = driver.find_element(By.LINK_TEXT, 'Kelas')
 kelas.click()
 
-tgl = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/table/tbody/tr[3]/td[2]/div[1]")
-# print(tgl.get_attribute('innerText'))
-ya = tgl.rect
-print(ya)
+sem = driver.find_element(By.PARTIAL_LINK_TEXT, 'Semester 2')
+sem.click()
 
+sem1 = driver.find_element(By.LINK_TEXT, 'Semester 1 - 2022/2023')
+sem1.click()
+
+des = driver.find_element(By.LINK_TEXT, 'Nov 2022')
+des.click()
+
+
+kalender = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/table/tbody")
+print(kalender.is_enabled())
+
+
+
+row = kalender.find_elements(By.TAG_NAME, 'tr')
+
+print(type(row))
+for i in row:
+    # print(i.text)
+    if tglMK[0] in i.text:
+        baris = i.find_elements(By.TAG_NAME, 'td')
+        break
+
+for i in baris:
+    if tglMK[0] in i.text:
+        tgl = i.find_elements(By.TAG_NAME, 'div')
+        break
+
+for i in tgl:
+    if tglMK[1] in i.text:
+        pencet = i.find_element(By.PARTIAL_LINK_TEXT, tglMK[1])
+        break
+pencet.click()
+
+# tandaiHadir = driver.find_element(By.LINK_TEXT, 'Tandai Hadir')
+# tandaiHadir.click()
+
+print("selesai")
+# pencet.click()
 # pencet = driver.find_element(By.PARTIAL_LINK_TEXT, ya)
 # action = ActionChains(driver)
 # action.double_click(pencet).perform()
